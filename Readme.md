33a0fc8497ed4382074b05ce71af80ba7d5dc8b5
Vladislav Rogozhin <rogojinvladislav@gmail.com>
2023-09-25 22:30:19 +0700

    version 1.0

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### Requires .env and team-members.js files
+
+### Requires pm2
+pm2 start index.js --name acqhamster
+pm2 restart acqhamster
---
+++ google-sheet.js
@@ -0,0 +1,53 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const dailyLeadsIds = require('./team-members');
+// {
+//     'Имя': 'Discord ID',
+// }
+
+module.exports.getDailyLead = async function getDaylyLead() {
+    const doc = new GoogleSpreadsheet(process.env.DOC_ID);
+    doc.useApiKey(process.env.GOOGLE_API_KEY);
+
+    await doc.loadInfo();
+
+    // Найти первый не скрытый лист
+    let index = 0;
+    while (index < doc.sheetCount) {
+        const candidateSheet = doc.sheetsByIndex[index];
+        if (candidateSheet.hidden) {
+            index++
+        } else {
+            break
+        }
+    }
+
+    const sheet = doc.sheetsByIndex[index];
+
+    await sheet.loadCells('A1:k2');
+
+    const dailyLeads = {}
+
+    for (let i = 1; i <= 10; i++) {
+        const numberDate = sheet.getCell(0, i).value
+        if (numberDate) {
+            const date = numberToDate(numberDate).toLocaleDateString("en-US");
+            const name = sheet.getCell(1, i).formattedValue;
+            dailyLeads[date] = name
+        }
+    }
+
+    function numberToDate(number) {
+        var date = new Date(number * 24 * 60 * 60 * 1000);
+        date.setFullYear(date.getFullYear() - 70);
+        date.setDate(date.getDate() - 1);
+        return (date);
+    }
+
+    const today = new Date().toLocaleDateString("en-US");
+
+    const dailyLeadName = dailyLeads[today];
+
+    const dailyLeadId = dailyLeadsIds[dailyLeadName]
+
+    return { dailyLeadName, dailyLeadId }
+}
